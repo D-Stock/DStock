e01844a984b728af38d1b268206dadb6392f6620
Ashish Nehra <56434281+Ashish-Nehra@users.noreply.github.com>
2021-08-04 12:05:50 +0530

    Update Buytoken.js

--- src/components/token_marketplace/Buytoken.js
+++ src/components/token_marketplace/Buytoken.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -7,6 +7,7 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import "../center.css";
+
 
 const useStyles = makeStyles ((theme) => ({
   root: {
@@ -26,25 +27,35 @@ const useStyles = makeStyles ((theme) => ({
   },
 }));
 
-export default function SimpleCard() {
+export default function SimpleCard(props) {
   const classes = useStyles();
+
+  let [Tokens,setTokens] = useState("");
+  const handleTokens = e => {
+    setTokens(e.target.value)
+  }
 
   return (
     <div class="centered">
     <Card className={classes.root} centered>
+    <form onSubmit={(event) => {
+        event.preventDefault()
+        props.buyToken(Tokens)
+        }} >
         <CardContent style={{backgroundColor: "lightblue"}}>
             <Typography variant="h5" component="h2">
             Buy Tokens Here
             <CardActions doubleSpacing={true}>
             </CardActions>
             </Typography>
-            <TextField id="outlined-basic" label="Tokens" variant="outlined" />
+            <TextField id="outlined-basic"  value={Tokens} label="Tokens" onChange={handleTokens}  label="Tokens" variant="outlined" />
             <CardActions doubleSpacing={true}>
             </CardActions>
-            <Button variant="contained" color="primary" disableElevation>
+            <Button type="submit" variant="contained" color="primary" disableElevation>
             Buy Now
             </Button>
         </CardContent>
+        </form>
         </Card>
     </div>
   );
